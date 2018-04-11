80461041a96f2b0b9319c55b1d597efa2aeab1fd
abutan <abutan@mail.ru>
2018-04-11 08:27:02 +0300

    Urls added to site

--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -4,6 +4,7 @@ return [
     'hostInfo' => $params['frontendHostInfo'],
     'enablePrettyUrl' => true,
     'showScriptName' => false,
+    'cache' => false,
     'rules' => [
         '' => 'site/index',
         'login' => 'auth/auth/login',
@@ -11,6 +12,20 @@ return [
         'signup' => 'auth/signup/request',
         'password' => 'auth/reset/request-password-reset',
         'reset' => 'auth/reset/reset-password',
+
+        'akcii-i-skidki' => 'sites/stock/index',
+        'akcii-i-skidki/<slug:[\w\-]+>' => 'sites/stock/node',
+
+        'bonusi' => 'sites/bonus/index',
+        'bonusi/<slug:[\w\-]+>' => 'sites/bonus/node',
+
+        'obyavlenia' => 'sites/notification/index',
+
+        'hits' => 'shop/catalog/hit',
+        'novinki' => 'shop/catalog/featured',
+        'predlogeniya' => 'sites/comment/index',
+
+        '<slug:[\w\-]+>' => 'sites/article/node',
 
         ['pattern' => 'yandex-market', 'route' => 'market/index', 'suffix' => '.xml'],
 
@@ -20,7 +35,14 @@ return [
 
         'cart' => 'shop/cart/index',
 
+        'catalog' => 'shop/catalog/index',
         ['class' => 'frontend\urls\CategoryUrlRules'],
         ['class' => 'frontend\urls\ProductUrlRules'],
+
+        'cabinet' => 'cabinet/default/index',
+        'cabinet/<_c:[\w\-]+>' => 'cabinet/<_c>/index',
+        'cabinet/<_c:[\w\-]+>/<id:\d+>' => 'cabinet/<_c>/view',
+        'cabinet/<_c:[\w\-]+>/<_a:[\w-]+>' => 'cabinet/<_c>/<_a>',
+        'cabinet/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => 'cabinet/<_c>/<_a>',
     ],
 ];
