d8c52d4733b607c87b1112930e43d027f3620a4d
abutan <abutan@mail.ru>
2018-05-21 14:31:43 +0300

    Login name changed

--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -7,7 +7,7 @@ return [
     'cache' => false,
     'rules' => [
         '' => 'site/index',
-        'login' => 'auth/auth/entry',
+        'entry' => 'auth/auth/entry',
         'logout' => 'auth/auth/logout',
         'signup' => 'auth/signup/request',
         'password' => 'auth/reset/request-password-reset',
