722e0ad61cfe40992075d1c4561c297ffa00d411
abutan <abutan@mail.ru>
2018-05-25 21:41:40 +0300

    Some styles changed

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -34,7 +34,7 @@ LtAppAsset::register($this);
             <?php if (Yii::$app->user->isGuest): ?>
                 <div class="enter col-sm-1 col-sm-offset-4">
                     <p>
-                        <i class="fa fa-sign-in-alt fa-2x" ></i>  <?= Html::a('Вход', ['/auth/auth/login']) ?>
+                        <i class="fa fa-sign-in-alt fa-2x" ></i>  <?= Html::a('<span>Вход</span>', ['/auth/auth/login']) ?>
                     </p>
                 </div><!--enter-->
 
